48ad8989bd4944901c73c57058771ea20c4bf27d
coolmadmadhu <madhu.sudan7925@gmail.com>
2020-05-03 00:58:45 +0530

    Fixed Image upload/edit/comment issues

--- src/main/java/ImageHoster/model/User.java
+++ src/main/java/ImageHoster/model/User.java
@@ -41,6 +41,10 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
     private List<Image> images = new ArrayList<>();
 
+    // mapped comments with user table
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<Comment> comments = new ArrayList<>();
+
     public Integer getId() {
         return id;
     }
@@ -79,6 +83,14 @@ public class User {
 
     public void setImages(List<Image> images) {
         this.images = images;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 }
 
